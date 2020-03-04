83cd7a156a2faec8ea8a7d51bcd446575c1fdc7b
Antonio <thetonybl@gmailc.om>
2020-03-04 12:29:31 -0600

    it’s now automatically saved to the filesystem.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+
 import {
   IonApp,
   IonIcon,
@@ -10,7 +11,7 @@ import {
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle, camera, images } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -33,6 +34,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { format } from 'util';
 
 const App: React.FC = () => (
   <IonApp>
@@ -50,8 +52,8 @@ const App: React.FC = () => (
             <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon icon={images} />
+            <IonLabel>Photos</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {camera, trash, close, logoAndroid} from 'ionicons/icons';
+import {IonGrid, IonCol, IonRow, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonImg } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import {usePhotoGalery} from '../hooks/usePhotoGalery';
+
 
 const Tab2: React.FC = () => {
+  const {photos, takePhoto} = usePhotoGalery();
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Galery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonGrid>
+          <IonRow>            
+            {photos.map((photo, index) => (
+              <IonCol size ="6" key = {index}>
+                  <IonTitle > {'Photo '+index+1}</IonTitle>
+                  <IonImg src = {photo.webViewPath}/>
+
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+        <IonFab vertical = "bottom" horizontal = "center" slot= "fixed">
+          <IonFabButton onClick = {()=> takePhoto()}>
+            <IonIcon icon = {camera}></IonIcon> 
+          </IonFabButton> 
+        </IonFab>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
+        
+      
     </IonPage>
   );
 };
